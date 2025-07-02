480b3bb22d09b84fb512df9d39e02fbd20819797
Hemraj Pant <123043728+hemrajpant69@users.noreply.github.com>
2025-07-02 21:47:47 +0545

    Update main.jsx

--- dropsafe-frontend/src/main.jsx
+++ dropsafe-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { UserProvider } from "./context/UserContext";
+import { UserProvider } from "./context/UserContext.jsx";
 import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
